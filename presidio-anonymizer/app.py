5d54d8133aaa2dc34b323573ac3c98db62ea18fe
Khadijah Bashir <232601918+kbashir2-ux@users.noreply.github.com>
2025-12-11 01:34:30 +0000

    ruff checked, import order fixed

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -6,11 +6,10 @@ from logging.config import fileConfig
 from pathlib import Path
 
 from flask import Flask, Response, jsonify, request
-from werkzeug.exceptions import BadRequest, HTTPException
-
 from presidio_anonymizer import AnonymizerEngine, DeanonymizeEngine
 from presidio_anonymizer.entities import InvalidParamError, OperatorConfig
 from presidio_anonymizer.services.app_entities_convertor import AppEntitiesConvertor
+from werkzeug.exceptions import BadRequest, HTTPException
 
 DEFAULT_PORT = "3000"
 
